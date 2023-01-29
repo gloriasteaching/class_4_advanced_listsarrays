4cbea5267fc5725925d9b5ba98b556660c4ccd50
gloriasteaching <47659579+gloriasteaching@users.noreply.github.com>
2023-01-29 20:09:30 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+let number_list = [
+1,
+2,
+3,
+4,
+5,
+6,
+7
+]
+let word_list = [
+"It's Monday",
+"It's Tuesday",
+"It's Wednesday",
+"It's Thursday",
+"It's Friday!",
+"It's Saturday",
+"It's Sunday"
+]
+info.setScore(number_list[randint(0, 6)])
+if (info.score() == 0) {
+    game.splash(word_list[0])
+} else if (info.score() == 1) {
+    game.splash(word_list[1])
+} else if (info.score() == 2) {
+    game.splash(word_list[2])
+} else if (info.score() == 3) {
+    game.splash(word_list[3])
+} else if (info.score() == 4) {
+    game.splash(word_list[4])
+} else if (info.score() == 5) {
+    game.splash(word_list[5])
+} else if (info.score() == 6) {
+    game.splash(word_list[6])
+}
+forever(function () {
+	
+})
