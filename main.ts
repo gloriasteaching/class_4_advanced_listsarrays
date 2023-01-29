e479d65502e49b5107a9578ca7c686d481d1a160
gloriasteaching <47659579+gloriasteaching@users.noreply.github.com>
2023-01-29 20:35:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,16 +25,18 @@ forever(function () {
     if (info.score() == 0) {
         game.splash("You are a " + word_list[0])
     } else if (info.score() == 1) {
-        game.splash(word_list[1])
+        game.splash("Your are a " + word_list[1])
     } else if (info.score() == 2) {
-        game.splash(word_list[2])
+        game.splash("You are a " + word_list[2], "")
     } else if (info.score() == 3) {
-        game.splash(word_list[3])
+        game.splash("You are a " + word_list[3])
     } else if (info.score() == 4) {
-        game.splash(word_list[4])
+        game.splash("You are a " + word_list[4])
     } else if (info.score() == 5) {
-        game.splash(word_list[5])
+        game.splash("You are a " + word_list[5])
     } else if (info.score() == 6) {
-        game.splash(word_list[6])
+        game.splash("You are a " + word_list[6])
+    } else {
+        game.splash("What the heck R U?")
     }
 })
