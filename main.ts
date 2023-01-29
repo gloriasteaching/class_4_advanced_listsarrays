4e9966b23b39e4a8f91308d51b3acbc6710d892a
gloriasteaching <47659579+gloriasteaching@users.noreply.github.com>
2023-01-29 20:30:11 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,19 +8,22 @@ let number_list = [
 7
 ]
 let word_list = [
-"It's Monday",
-"It's Tuesday",
-"It's Wednesday",
-"It's Thursday",
-"It's Friday!",
-"It's Saturday",
-"It's Sunday"
+"meatball!",
+"potato couch!",
+"giant pear!",
+"stinky cabbage",
+"shoe steak!",
+"poopy burger!",
+"melancholic pug!"
 ]
 forever(function () {
-    info.setScore(number_list[randint(0, 6)])
+    game.splash("Hi!", "Do you know who you are?")
+    game.splash("Let's find out!")
+    game.splash("Chose 0-6")
+    info.setScore(game.askForNumber("", 1))
     pause(1000)
     if (info.score() == 0) {
-        game.splash(word_list[0])
+        game.splash("You are a " + word_list[0])
     } else if (info.score() == 1) {
         game.splash(word_list[1])
     } else if (info.score() == 2) {
